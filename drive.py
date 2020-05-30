467511c5b2d2ccfbbc706849de2c52247c51ddcc
sbienaime <stevebienaime@gmail.com>
2020-05-30 10:49:27 -0400

    fixing typos

--- drive.py
+++ drive.py
@@ -4,10 +4,11 @@ import cars
 mycar = cars.Cars(4,'blue','Honda','Civic')
 
 print("mycar is an instance aka  an object  of the Cars class defined in cars.py.  \n \n " )
-print("I can access the attributes and methods/functions of this object using dot notation as seen below. \n \n  ")
+print("I can access the attributes and methods/functions of this object using dot notation"
+      " as seen below. \n \n  ")
 
 
-print("My car is a " + mycar.color+ " "+mycar.make+" "+mycar.model )
+print("My car is a " + mycar.color + " "+mycar.make+" "+mycar.model )
 
 mycar.move()
 mycar.accelerate()
@@ -15,13 +16,16 @@ mycar.Break()
 
 
 
-print("Once again, a class is a definition of a type of a object, you can have many instances or versions of a class which have different attributes ( these are versions or instances are objects of that class.\n \n ")
+print("Once again, a class is a definition of a type of a object, you can have many"
+      " instances or versions of a class which have different attributes ( these are"
+      " versions or instances are objects of that class.\n \n ")
 
 print("Below is another instance/object of this same class.\n \n ")
 
 ans = 'N'
 while (ans !="y" ) :
-    ans = input("Are you ready to see an other instance of the same class, type the letter (y) when youre ready  ")
+    ans = input("Are you ready to see another instance of the same class, "
+                "type the letter (y) when youre ready  ")
 
 #this is another instance or object of the Cars class
 myOtherCar = cars.Cars(5,'green','Toyota','camry')
@@ -33,7 +37,8 @@ print(' \n ')
 
 
 
-print("My other car ( another object of the same class)  is a " + myOtherCar.color+ " "+myOtherCar.make+" "+myOtherCar.model )
+print("My other car ( another object of the same class)  is"
+      " a " + myOtherCar.color+ " "+myOtherCar.make+" "+myOtherCar.model )
 
 
 print(' \n ')
@@ -42,5 +47,8 @@ myOtherCar.move()
 myOtherCar.accelerate()
 myOtherCar.Break()
 
-print("This other object has a different color and make  and model etc but it could have had the same attribues of the previous object but they would have still been two different objects, "
-      "just like if you had two cars with the same make model color etc in real life")
+print("This other object has a different color and make "
+      " and model etc but it could have had the same attribues of the previous object but "
+      "they would have still been two different objects, "
+      "just like if you had two cars with the same make model "
+      "color etc in real life")
